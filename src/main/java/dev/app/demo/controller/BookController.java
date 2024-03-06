c5e36a3ec33952058f963f27c179e3f57091317e
Anish <anish.panthi41@gmail.com>
2024-03-06 13:47:08 -0600

    Completed CRUD Operation

--- src/main/java/dev/app/demo/controller/BookController.java
+++ src/main/java/dev/app/demo/controller/BookController.java
@@ -3,13 +3,13 @@ package dev.app.demo.controller;
 import dev.app.demo.entity.Book;
 import dev.app.demo.repository.BookRepository;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Log4j2
 public class BookController {
 
   private final BookRepository bookRepository;
@@ -26,5 +26,27 @@ public class BookController {
   @GetMapping("/books/{id}")
   public Book getBookById(@PathVariable Integer id) {
     return bookRepository.findById(id).orElse(null);
+  }
+
+  @PostMapping("/books")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void saveBook(@RequestBody Book book) {
+    bookRepository.save(book);
+  }
+
+  @PutMapping("/books/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void updateBook(@RequestBody Book book, @PathVariable Integer id) {
+    if (bookRepository.findById(id).isEmpty()) {
+      log.info("Book not found with id: {}", id);
+      return;
+    }
+    bookRepository.save(book);
+  }
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @DeleteMapping("/books/{id}")
+  public void deleteBook(@PathVariable Integer id) {
+    bookRepository.findById(id).ifPresent(bookRepository::delete);
   }
 }
--- src/main/java/dev/app/demo/repository/BookRepository.java
+++ src/main/java/dev/app/demo/repository/BookRepository.java
@@ -6,9 +6,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Log4j2
 public class BookRepository {
 
   private final List<Book> books = new ArrayList<>();
@@ -20,6 +22,7 @@ public class BookRepository {
         new Book(2, "Python", "Guido van Rossum", "Python Programming", LocalDate.of(2005, 2, 20)));
     books.add(
         new Book(3, "C++", "Bjarne Stroustrup", "C++ Programming", LocalDate.of(2000, 10, 14)));
+    log.info("Initial Book Size: {}", books.size());
   }
 
   public List<Book> findAll() {
@@ -28,5 +31,17 @@ public class BookRepository {
 
   public Optional<Book> findById(Integer id) {
     return books.stream().filter(b -> b.id().equals(id)).findFirst();
+  }
+
+  public void save(Book book) {
+    books.removeIf(b -> b.id().equals(book.id()));
+    books.add(book);
+    log.info("After save, books size is {}", books.size());
+  }
+
+  public void delete(Book book) {
+    log.info("Size of books before delete: {}", books.size());
+    books.removeIf(b -> b.id().equals(book.id()));
+    log.info("Size of books after delete: {}", books.size());
   }
 }
