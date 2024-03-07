d25ca8b86d86036df4d455046a44db1d36aaf08e
Anish <anish.panthi41@gmail.com>
2024-03-07 17:03:55 -0600

    WIP

--- src/main/java/dev/app/demo/config/SecurityConfig.java
+++ src/main/java/dev/app/demo/config/SecurityConfig.java
@@ -1,19 +1,26 @@
-package dev.app.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
-        a -> a.requestMatchers("/actuator/metrics").permitAll().anyRequest().authenticated());
-    return http.build();
-  }
-}
+//package dev.app.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http.authorizeHttpRequests(
+//        a ->
+//            a.requestMatchers(HttpMethod.GET, "/api/books")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated());
+//    http.csrf(AbstractHttpConfigurer::disable);
+//    return http.build();
+//  }
+//}
