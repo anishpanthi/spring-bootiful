d416bea0598cbaa4849853ccebd505da00ecbad9
Anish Panthi <anish.panthi@gmail.com>
2024-03-07 20:36:01 -0600

    WIP...

--- src/main/java/dev/app/demo/config/SecurityConfig.java
+++ src/main/java/dev/app/demo/config/SecurityConfig.java
@@ -1,26 +1,25 @@
-//package dev.app.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//  @Bean
-//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http.authorizeHttpRequests(
-//        a ->
-//            a.requestMatchers(HttpMethod.GET, "/api/books")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated());
-//    http.csrf(AbstractHttpConfigurer::disable);
-//    return http.build();
-//  }
-//}
+package dev.app.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        a ->
+            a.requestMatchers("/unsecured/api")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+    http.formLogin(Customizer.withDefaults());
+    http.csrf(AbstractHttpConfigurer::disable);
+    return http.build();
+  }
+}
