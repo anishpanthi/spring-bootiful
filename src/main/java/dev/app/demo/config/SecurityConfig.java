ea50ff5416ce8139d00d35b0dca95ef673805b57
Anish <anish.panthi41@gmail.com>
2024-03-11 10:55:18 -0500

    Updated Security

--- src/main/java/dev/app/demo/config/SecurityConfig.java
+++ src/main/java/dev/app/demo/config/SecurityConfig.java
@@ -1,14 +1,27 @@
 package dev.app.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  @Value("${spring.security.user.name}")
+  private String username;
+
+  @Value("${spring.security.user.password}")
+  private String password;
+
+  @Value("${spring.security.authorities}")
+  private String[] authorities;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,5 +31,11 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new InMemoryUserDetailsManager(
+        User.withUsername(username).password(password).authorities(authorities).build());
   }
 }
